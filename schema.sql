c17ac3e622be6615bc1795996466b6a4231a4d12
Alex <35956101+DUHsoALEX@users.noreply.github.com>
2020-05-11 14:43:16 +1000

    simple db schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE Wikis (
     wiki_id             integer,
     wiki_url            text not null,
     hub                 text not null,
-    isUCP               text not null,
+    is_UCP              text not null,
     primary key (wiki_id)
 );
 
@@ -25,18 +25,18 @@ CREATE TABLE User_info (
 );
 
 CREATE TABLE Revisions (
-    revision_id     integer,
-    page_id         integer not null,
-    user_id         integer not null,
-    wiki_id         integer not null,
-    timestamp       text not null,
-    comment         text not null,
-    byte_length     text not null,
-    flags           text not null,
-    text            text not null,
-    parent          text not null,
-    diff            text not null,
-    namespace       text not null,
+    revision_id         integer,
+    page_id             integer not null,
+    user_id             integer not null,
+    wiki_id             integer not null,
+    timestamp           text not null,
+    comment             text not null,
+    byte_length         text not null,
+    flags               text not null,
+    text                text not null,
+    parent              text not null,
+    diff                text not null,
+    namespace           text not null,
     foreign key (page_id) references Pages(page_id),
     foreign key (user_id) references User_info(user_id),
     foreign key (wiki_id) references Wikis(wiki_id),
@@ -44,10 +44,10 @@ CREATE TABLE Revisions (
 );
 
 CREATE TABLE Categories (
-    revision_id     integer not null,
-    damaging        text,
-    spam            text,
-    goodfaith       text,
+    revision_id         integer not null,
+    damaging            text,
+    spam                text,
+    goodfaith           text,
     foreign key (revision_id) references Revisions(revision_id),
     primary key (revision_id)
 );
