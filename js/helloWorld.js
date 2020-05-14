4f387e622d06917e72c0c3ac506cd314a1281799
Alex <35956101+DUHsoALEX@users.noreply.github.com>
2020-05-15 00:48:41 +1000

    fixed indentation

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -2,10 +2,10 @@ function showDiff(wiki, revid) {
    console.log('https://cors-anywhere.herokuapp.com/' + wiki + 'api.php?action=query&prop=revisions&revids=' + revid + '&rvprop=ids|timestamp|flags|comment|user|content&rvdiffto=prev&format=json');
 
    $.get('https://cors-anywhere.herokuapp.com/' + wiki + 'api.php?action=query&prop=revisions&revids=' + revid + '&rvprop=ids|timestamp|flags|comment|user|content&rvdiffto=prev&format=json').done(function(d) {
-      console.log(d);
-      $('body').html('<div id="hello"></div>');
-      $('#hello').append('<table></table>');
-      $('#hello').append(d.query.pages[Object.keys(d.query.pages)[0]].revisions[0].diff['*']);
+        console.log(d);
+        $('body').html('<div id="hello"></div>');
+        $('#hello').append('<table></table>');
+        $('#hello').append(d.query.pages[Object.keys(d.query.pages)[0]].revisions[0].diff['*']);
    });
 }
 
