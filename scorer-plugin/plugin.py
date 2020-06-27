359765eed1cb683e99d3b4727ceecc10064482b0
Kris <kris.choy1@gmail.com>
2020-06-28 02:25:51 +1000

    Get features

--- scorer-plugin/plugin.py
+++ scorer-plugin/plugin.py
@@ -9,6 +9,12 @@ import supybot.log as log
 import requests
 import re
 import json
+import os
+import sys
+from editquality.feature_lists.enwiki import damaging
+from revscoring.extractors import api
+from revscoring.errors import TextDeleted, RevisionNotFound
+import mwapi
 
 try:
     from supybot.i18n import PluginInternationalization
@@ -23,15 +29,20 @@ class Cynthia(callbacks.Plugin):
     Cynthia: Revscoring to Discord plugin
     """
     def __init__(self, irc):
+        
         self.__parent = super(Cynthia, self)
         self.__parent.__init__(irc)
         self.log = log.getPluginLogger('Cynthia')
 
-        with open('config.json', 'r') as f:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + '/config.json', 'r') as f:
             self.config = json.load(f)
         
         self.re_edit = re.compile(ur'^\x0314\[\[\x0307([^\]]+)\x0314\]\]\x034 ([!NBM]*)\x0310 \x0302https?:\/\/([a-z0-9-.]+)\.(fandom\.com|wikia\.(?:com|org)|(?:wikia|fandom)-dev\.(?:com|us|pl))\/(?:([a-z-]+)\/)?index\.php\?(\S+)\x03 \x035\*\x03 \x0303([^\x03]+)\x03 \x035\*\x03 \(\x02?(\+|-)(\d+)\x02?\) \x0310(.*)$')
         # page, flags, wiki, domain, language, params, user, sign, num, summary
+
+        self.damaging = damaging
+
         
     def get_wiki_and_diff(self, msg):
         """
@@ -50,17 +61,25 @@ class Cynthia(callbacks.Plugin):
                 diff = params['diff']
             elif 'oldid' in params:
                 diff = params['oldid']
-            return (m.group(3) + '.' + m.group(4), diff)
+            return ('https://' + m.group(3) + '.' + m.group(4), diff)
+        return (None, None)
+
+    def get_features(self, wiki, diff):
+        session = mwapi.Session(wiki, api_path='/api.php', 
+                    user_agent='Cynthia - Vandalism detection bot, @noreplyz')
+        api_extractor = api.Extractor(session)
+        print(list(api_extractor.extract(int(diff), self.damaging)))
 
     def doPrivmsg(self, irc, msg):
         if irc.network == self.config['irc']['network'] and msg.args[0] == self.config['irc']['rc']:
-            wiki, diff = get_wiki_and_diff(msg.args[1])
+            wiki, diff = self.get_wiki_and_diff(msg.args[1])
 
             if wiki and diff:
-                self.log.info(wiki + diff)
+                # self.log.info(wiki + diff)
+                self.log.info(self.get_features(wiki, diff))
 
 Class = Cynthia
 
 if __name__ == "__main__":
-    new = Cynthia("Test")
+    new = Cynthia("Cynthia")
     print(new)
